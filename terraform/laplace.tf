47f5cb38573d964d95702d71dd046112f281f9d1
Mirza Hilmi <dev@mrz.my.id>
2024-10-03 13:55:26 +0700

    refactor(laplace): use global superkey

--- terraform/laplace.tf
+++ terraform/laplace.tf
@@ -4,19 +4,19 @@ variable "projects" {
     host_segment = number
   }))
   default = {
-    career-path-ai = {
+    "career-path-ai" = {
       pubkey       = ""
       host_segment = 2
     }
-    computy-ai = {
+    "computy-ai" = {
       pubkey       = ""
       host_segment = 3
     }
-    mavis-summarizer = {
+    "mavis-summarizer" = {
       pubkey       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6qOQwZH6BEUIHVLrpjnBhwWHduqxUFeNkr5MOneD2B williamchen1506@gmail.com"
       host_segment = 4
     }
-    whale-ai = {
+    "whale-ai" = {
       pubkey       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgVDBg82J/Gm49NMq/Nz29FxUXs+Wln77BKluaUeHEj radityanalaa@gmail.com"
       host_segment = 5
     }
@@ -61,8 +61,7 @@ resource "proxmox_vm_qemu" "laplace" {
   ciuser     = var.cloud_init_user
   cipassword = var.cloud_init_passwd
   sshkeys    = <<EOF
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPB4xBL5wKhqusJipjrsV+Peq1D7ge1G2EHJtaSLL5p2 dev@mrz.my.id
-    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj6EoGomhmaGhAGRncamehO73AeuLi8F2JU5ZaKJ78WzroLDk0IEAbf28YfCuSHJmAquWOFxA/h3Ch2O11KRO4YjGLraDEUW9D/Qolaij3TiXYqOGReYk5BQfVc5OmWK35BJhRsX4+4yAHrzUxM/lli90kDrlo21RSaaCsYbL+OZ59tngGJQX+5bpXcsqqTLs7teooyEVWULf3n2h/Qszu04KzL+FqoZG0LaeZPMqNiwno5Kvj6oh5JTMmrGQRL8rV/sSXMPsCiYGTDAePqEVTHM0WjLDFMiMuv9x1+mDwCSYT6mLWeYJhY8WiPvrM2yNQMnvTyuWpkRU7ap2OB/jGZ8cKMWTPytcfI4eG/+tfptORzUgmjaUQQ3SBPQr1IF57Mevs6LTH5q/I/bb1oOlEp1ouRckQoYg9ubFTJQ2zZPcm+RxTfT8l9EaN25mvkSzjs16FJAfPkH0UsziD13aiDBT6ppd7gp1nUDGX3xwP8Nr2HDoXP2HafiWpS05csK8= msi@DESKTOP-57RMF98
+    ${var.superkey}
     ${each.value.pubkey}
   EOF
 }
