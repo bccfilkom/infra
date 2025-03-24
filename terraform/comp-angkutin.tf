fc29c11e2ba651091b76698e77a4b287f4097e9f
yogarn <radityanalaa@gmail.com>
2025-03-24 07:58:43 +0700

    `feat:` Add competition vm

---
+++ terraform/comp-angkutin.tf
@@ -0,0 +1,42 @@
+resource "proxmox_vm_qemu" "comp-angkutin" {
+  name        = "angkutin"
+  desc        = "Gelatik Smart City - Angkutin"
+  target_node = "bcc-dev"
+  clone       = "ubuntu24-cloud"
+  agent       = 0
+  full_clone  = true
+
+  os_type = "cloud-init"
+  cores   = 1
+  sockets = 2
+  memory  = 2048
+  numa    = true
+
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage = "local-lvm"
+          size    = 16
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  ipconfig0  = "ip=192.168.1.5/24,gw=192.168.1.1"
+  ciuser     = var.cloud_init_user
+  cipassword = var.cloud_init_passwd
+  sshkeys    = <<EOF
+        ${var.superkey}
+    EOF
+}
